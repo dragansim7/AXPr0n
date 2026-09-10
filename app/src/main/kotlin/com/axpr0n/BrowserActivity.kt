9699e11d836d066dfc518b5201e419bea49508a3
dragansim7 <136042512+dragansim7@users.noreply.github.com>
2026-09-10 18:54:22 +1000

    Clean up BrowserActivity and remove unused imports

--- app/src/main/kotlin/com/axpr0n/BrowserActivity.kt
+++ app/src/main/kotlin/com/axpr0n/BrowserActivity.kt
@@ -5,8 +5,6 @@ import android.view.KeyEvent
 import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
-import android.widget.ProgressBar
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.axpr0n.databinding.ActivityBrowserBinding
 
